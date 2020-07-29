3e5a16ba585af8ed3b9e6e9cbd33d628b05517c6
yukiyaxxxx <59367678+yukiyaxxxx@users.noreply.github.com>
2020-07-29 15:10:20 +0900

    inquiry バリデーション

--- htdocs/server/resources/views/inquiry/form.blade.php
+++ htdocs/server/resources/views/inquiry/form.blade.php
@@ -1,24 +1,50 @@
+<style>
+    .error {
+        color: #f00;
+    }
+</style>
+
 <form action="{{ $actionUrl }}" method="POST">
 
     @csrf
 
     <label>種別</label>
     <input type="text" name="type" value="{{ old('type') }}">
+    @error('type')
+    <p class="error">
+        {{ $message }}
+    </p>
+    @enderror
 
     <br><br>
 
     <label>氏名</label>
     <input type="text" name="name" value="{{ old('name') }}">
+    @error('name')
+    <p class="error">
+        {{ $message }}
+    </p>
+    @enderror
 
     <br><br>
 
     <label>メールアドレス</label>
     <input type="text" name="email" value="{{ old('email') }}">
+    @error('email')
+    <p class="error">
+        {{ $message }}
+    </p>
+    @enderror
 
     <br><br>
 
     <label>本文</label>
-    <textarea name="body" >{{ old('body') }}</textarea>
+    <textarea name="body">{{ old('body') }}</textarea>
+    @error('body')
+    <p class="error">
+        {{ $message }}
+    </p>
+    @enderror
 
     <br><br>
 
