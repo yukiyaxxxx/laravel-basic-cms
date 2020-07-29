200bcf1177ad42bb5c170b7adb1dda45aea4dc9a
yukiyaxxxx <59367678+yukiyaxxxx@users.noreply.github.com>
2020-07-29 13:45:43 +0900

    inquiry メール送信テスト

--- htdocs/server/tests/Feature/Controllers/InquiryControllerTest.php
+++ htdocs/server/tests/Feature/Controllers/InquiryControllerTest.php
@@ -2,22 +2,65 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Mails\Inquiry\AdminMail;
+use App\Mails\Inquiry\CustomerMail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class InquiryControllerTest extends TestCase
 {
-
     public function testForm()
     {
         $response = $this->get(route('inquiry.form'));
         $response->assertStatus(200);
     }
 
+    public function testConfirm()
+    {
+        \Mail::fake();
+
+        $params = [
+            'type'       => 'type',
+            'email'      => 'email@example.com',
+            'name'       => 'name',
+            'body'       => 'body',
+            'actionType' => 'confirm' // 確認画面アクション
+        ];
+
+        // サンクスメール検証
+        $response = $this->post(route('inquiry.post'), $params);
+        $response->assertStatus(200);
+        $response->assertSee(route('inquiry.post'));
+    }
+
     public function testPost()
     {
-        $this->markTestIncomplete('TODO:');
+        \Mail::fake();
+
+        $params = [
+            'type'       => 'type',
+            'email'      => 'email@example.com',
+            'name'       => 'name',
+            'body'       => 'body',
+            'actionType' => 'send' // 送信アクション
+        ];
+
+        // サンクスメール検証
+        $response = $this->post(route('inquiry.post'), $params);
+        $response->assertStatus(302);
+        $response->assertRedirect(route('inquiry.thanks'));
+
+        \Mail::assertSent(AdminMail::class, 1);
+        \Mail::assertSent(AdminMail::class, function ($mail) {
+            return $mail->hasTo(config('env.admin_email'));
+        });
+
+        // 管理者メール検証
+        \Mail::assertSent(CustomerMail::class, 1);
+        \Mail::assertSent(CustomerMail::class, function ($mail) {
+            return $mail->hasTo('email@example.com');
+        });
     }
 
     public function testThanks()
